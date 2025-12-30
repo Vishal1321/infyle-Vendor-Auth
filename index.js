5d88f6ebd3511ca1d1582acb219c64c9bf516ebb
Vishal1321 <vishalthakur2318@gmail.com>
2025-12-30 09:42:12 +0530

    intial setup

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");4
+const cors = require("cors");
+require("dotenv").config();
+
+const app=express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/",(req,res)=>{
+    res.send("INFYLE Vendor Api running");
+
+});
+
+const PORT =process.env.PORT||5000;
+app.listen(PORT,()=>{
+    console.log(`server runnning on port ${PORT}`)
+});
